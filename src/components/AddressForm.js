8c28d75a8c18b7cbdc9a0d2e670a1ecab05f9d9f
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2020-02-17 15:40:25 -0500

    useReducer ftw

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -2,20 +2,20 @@ import React from "react";
 
 import { FormControl, InputLabel, Input } from "@material-ui/core";
 
-function AddressForm({ onChange }) {
+function AddressForm({ onChange, addressState }) {
   return (
     <div className="Form">
       <FormControl>
         <div className="FormField">
-          <InputLabel htmlFor="line-1">Address</InputLabel>
-          <Input id="line-1" type="text" onChange={onChange} />
+          <InputLabel htmlFor="line_1">Address</InputLabel>
+          <Input id="line_1" type="text" onChange={onChange} />
         </div>
       </FormControl>
 
       <FormControl>
         <div className="FormField">
-          <InputLabel htmlFor="line-2">Line 2</InputLabel>
-          <Input id="line-2" type="text" onChange={onChange} />
+          <InputLabel htmlFor="line_2">Line 2</InputLabel>
+          <Input id="line_2" type="text" onChange={onChange} />
         </div>
       </FormControl>
       <FormControl>
--- src/components/RatingInformationForm.js
+++ src/components/RatingInformationForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 
 import { FormControl, Button } from "@material-ui/core";
 import AddressForm from "./AddressForm";
@@ -8,14 +8,12 @@ function RatingInformationForm() {
   //TODO: validation on inputs + Styling.
   const [first_name, setFirstName] = useState("");
   const [last_name, setLastName] = useState("");
-  const [address, setAddress] = useState({});
-  const buildAddressObject = e => {
-    e.persist();
-    setAddress(
-      Object.assign({}, address, {
-        [e.target.id]: e.target.value
-      })
-    );
+  function addressReducer(state, { field, value }) {
+    return { ...state, [field]: value };
+  }
+  const [addressState, dispatch] = useReducer(addressReducer, {});
+  const onAddressChange = e => {
+    dispatch({ field: e.target.id, value: e.target.value });
   };
 
   const onChange = e => {
@@ -25,24 +23,38 @@ function RatingInformationForm() {
       : setLastName(e.target.value);
   };
   const handleSubmit = e => {
-    //build the post request body here, send request to: https://fed-challenge-api.sure.now.sh/api/v1/quotes
     const request = {
       first_name,
       last_name,
-      address
+      address: addressState
     };
 
-    console.log(request, "the request");
+    fetch("https://fed-challenge-api.sure.now.sh/api/v1/quotes", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(request)
+    })
+      .then(response => response.json())
+      .then(res => {
+        console.log(res, "the post response");
+      })
+      .catch(err => {
+        console.log(err, "the error");
+      });
   };
+
   return (
     <div className="Form">
       <FormControl>
         <FormField label="First Name" id="first_name" onChange={onChange} />
       </FormControl>
       <FormControl>
-      <FormField label="Last Name" id="last_name" onChange={onChange} />
+        <FormField label="Last Name" id="last_name" onChange={onChange} />
       </FormControl>
-      <AddressForm address={address} onChange={buildAddressObject} />
+      <AddressForm onChange={onAddressChange} addressState={addressState} />
       <Button type="submit" color="primary" onClick={handleSubmit}>
         Submit
       </Button>
