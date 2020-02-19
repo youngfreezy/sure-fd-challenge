c475ed9839433f3f9d4252b14b5506f29727f494
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2020-02-18 19:43:17 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//sure-fd-challenge/precache-manifest.7be04edd38ea0898a4bbdd08e58510d1.js"
+  "/sure-fd-challenge/precache-manifest.53d9c1345593d58f363d94fd82cb980b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//sure-fd-challenge/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sure-fd-challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
